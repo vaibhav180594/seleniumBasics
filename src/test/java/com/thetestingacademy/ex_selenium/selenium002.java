2e1722f7d12a00264fdadf1155ad86d350f8aac6
vaibhavshinde185 <vaibhav.shinde185@gmail.com>
2024-10-14 12:20:06 +0530

    commit

--- src/test/java/com/thetestingacademy/ex_selenium/selenium002.java
+++ src/test/java/com/thetestingacademy/ex_selenium/selenium002.java
@@ -2,11 +2,13 @@ package com.thetestingacademy.ex_selenium;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class selenium001 {
+public class selenium002 {
     public static void main(String[] args) {
 
-        ChromiumDriver driver = new ChromeDriver();
+       // ChromiumDriver driver = new ChromeDriver();
+        FirefoxDriver driver = new FirefoxDriver();
         driver.get("https://app.vwo.com");
         driver.navigate().to("https://app.vwo.com");
         //driver.close();
